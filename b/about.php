0dfa519cafa84ef0bd7b3b3788db617d99119ed9
roberto-carlos12 <55759038+roberto-carlos12@users.noreply.github.com>
2019-10-01 15:38:55 -0300

    Update about.php

--- b/about.php
+++ b/about.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<?php include 'styleABOUT.css'?>
+  <link  rel="stylesheet" type="text/css" href="http://realmotos.epizy.com/styleABOUT.css">
 
 <div class="w3-container" style="padding:2% 5%" id="about">
   <h3 class="w3-center">SOBRE REAL MOTOS</h3>

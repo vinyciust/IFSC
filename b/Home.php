bd9cd401f6dffdb85be5129f270332ec9ab0e65e
roberto-carlos12 <55759038+roberto-carlos12@users.noreply.github.com>
2019-10-01 14:31:30 -0300

    Update Home.php

--- b/Home.php
+++ b/Home.php
@@ -5,7 +5,8 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
    
-   include 'stylecarroselHOME.css';
+   
+  <?php include 'stylecarroselHOME.css'; ?>
  
 <div id="demo" class="carousel slide" data-ride="carousel">
   <ul class="carousel-indicators">

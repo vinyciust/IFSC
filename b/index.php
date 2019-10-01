3484d74f80587e89cbd154d6764de17ad18fd904
gustavosilvani <gustavosilvani@hotmail.com>
2019-10-01 15:57:36 -0300

    Update index.php

--- b/index.php
+++ b/index.php
@@ -9,6 +9,19 @@
 
 <link  rel="stylesheet" type="text/css" href="http://realmotos.epizy.com/styleINDEX.css">
 
+<style>
+		#carrosel{
+		padding-top: 3%;
+		padding-left: 3%;
+		padding-bottom: 3%;
+		width: 50%;
+		height: 50%;
+		background-color: black;		
+	}
+
+	#home{background-color: black;}
+
+</style>
 
 <!-- <?php include 'MontaHtml.php'; ?> -->
 

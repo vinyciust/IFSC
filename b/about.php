52189985724e0f179de4ca7e2aebcdd2eeb87ee4
Gustavo Silvani <gustavosilvani@hotmail.com>
2019-10-29 16:31:51 -0300

    Update about.php

--- b/about.php
+++ b/about.php
@@ -1,4 +1,4 @@
-<!-- About Section -->
+<section><!-- About Section -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
@@ -32,3 +32,4 @@
     </div>
   </div>
 </div>
+</section>

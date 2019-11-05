73935d149b5c095fa1ba2c69f6ba8a740b444d91
Gustavo Silvani <gustavosilvani@hotmail.com>
2019-11-05 14:54:03 -0300

    Update menu.php

--- b/menu.php
+++ b/menu.php
@@ -27,13 +27,12 @@
 <header id="Menu">
 
 
-<nav class="topnav" id="myTopnav">
-  <a id="logo"><i class="fa"><img src="https://imgur.com/a/fIQC0sK"/></i></a> <!-- Botar a logo via html e editar via css -->
-  <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Inicio</a> 
-  <a href="#"><i class="fa fa-fw fa-info-circle" ></i> Sobre</a> 
-  <a href="#"><i class="fa fa-fw fa-envelope"></i> Contatos</a> 
+<div class="navbar">
+ <a id="logo"><i class="fa"><img src="https://imgur.com/a/fIQC0sK"/></i></a> <!-- Botar a logo via html e editar via css -->
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
   <div class="dropdown">
-    <button class="dropbtn">Serviço 
+    <button class="dropbtn">Dropdown 
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
@@ -41,22 +40,9 @@
       <a href="#">Link 2</a>
       <a href="#">Link 3</a>
     </div>
-     </div> 
-    <a href="#" id="login"><i class="fa fa-fw fa-user"></i> Login</a>
-    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-    
- 
-</nav>
-<script>
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-</script>
+  </div> 
+  <a href="#" id="login"><i class="fa fa-fw fa-user"></i> Login</a>
+
+</div>
 
 </header>

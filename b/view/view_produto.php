35874a196dc14328600dc9f40a2bf286c535bffd
roberto-carlos12 <55759038+roberto-carlos12@users.noreply.github.com>
2019-11-05 15:08:06 -0300

    Update view_produto.php

--- b/view/view_produto.php
+++ b/view/view_produto.php
@@ -14,7 +14,7 @@
 	<form method="POST" action="../controller/produto_controller.php" name="form1">
 
 		Nome: 					<input type="text" name="nome"><br />
-		Descricao:				    <input type="text" name="descricao" ><br />
+		Descricao:				<input type="text" name="descricao" ><br />
 		Valor:    				<input type="text" name="valor"><br />		
 				
 

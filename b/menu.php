1d228e2b2503c141c390bd3dd8706d934bf52373
Gustavo Silvani <gustavosilvani@hotmail.com>
2019-11-05 17:58:07 -0300

    Create menu.php

---
+++ b/menu.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+  <title>Real Motos</title>
+  <meta charset="utf-8">  
+
+
+<link  rel="stylesheet" type="text/css" href="https://realmotosserver.000webhostapp.com/htdocs/styleINDEX.css">
+<link  rel="stylesheet"  href="https://realmotosserver.000webhostapp.com/htdocs/styleMENU.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<link  rel="stylesheet" type="text/css" href="https://realmotosserver.000webhostapp.com/htdocs/stylecarroselHOME.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://realmotosserver.000webhostapp.com/htdocs/styleABOUT.css">
+<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,700|Muli:300,400" rel="stylesheet">
+<link  rel="stylesheet" type="text/css" href="https://realmotosserver.000webhostapp.com/htdocs/styleFooter.css">
+
+</head>
+
+<body style="color: black">
+
+<header id="Menu">
+
+
+<nav class="navbar">
+ <a id="logo"><i class="fa"><img src="https://imgur.com/a/fIQC0sK"/></i></a> <!-- Botar a logo via html e editar via css -->
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
+  <div class="dropdown">
+    <button class="dropbtn">Dropdown 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div> 
+  <a href="#" id="login"><i class="fa fa-fw fa-user"></i> Login</a>
+
+</nav>
+
+
+
+</header>

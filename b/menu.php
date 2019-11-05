dfe117f776cb49c15a687961cf152bb147a14594
roberto-carlos12 <55759038+roberto-carlos12@users.noreply.github.com>
2019-11-05 14:35:00 -0300

    Update menu.php

--- b/menu.php
+++ b/menu.php
@@ -18,7 +18,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="https://realmotos.epizy.com/styleABOUT.css">
 <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,700|Muli:300,400" rel="stylesheet">
-
+<link  rel="stylesheet" type="text/css" href="http://realmotos.epizy.com/styleFooter.css">
 
 </head>
 
@@ -27,16 +27,13 @@
 <header id="Menu">
 
 
-
-
-
-<div class="navbar">
-    <a id="logo"><i class="fa"><img src="https://imgur.com/a/fIQC0sK"/></i></a> <!-- Botar a logo via html e editar via css -->
-
-  <a href="#home">Home</a>
-  <a href="#news">News</a>
+<nav class="topnav" id="myTopnav">
+  <a id="logo"><i class="fa"><img src="https://imgur.com/a/fIQC0sK"/></i></a> <!-- Botar a logo via html e editar via css -->
+  <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Inicio</a> 
+  <a href="#"><i class="fa fa-fw fa-info-circle" ></i> Sobre</a> 
+  <a href="#"><i class="fa fa-fw fa-envelope"></i> Contatos</a> 
   <div class="dropdown">
-    <button class="dropbtn">Dropdown 
+    <button class="dropbtn">Serviço 
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
@@ -44,11 +41,13 @@
       <a href="#">Link 2</a>
       <a href="#">Link 3</a>
     </div>
-  </div>
-      <a href="#" id="login"><i class="fa fa-fw fa-user"></i> Login</a>
+     </div> 
+    <a href="#" id="login"><i class="fa fa-fw fa-user"></i> Login</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+    
  
-</div>
-
+</nav>
 <script>
 function myFunction() {
   var x = document.getElementById("myTopnav");

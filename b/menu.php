8e42f5332c21593fa7dce51a18fad3fee4ad8b89
Gustavo Silvani <gustavosilvani@hotmail.com>
2019-11-05 14:29:50 -0300

    Update menu.php

--- b/menu.php
+++ b/menu.php
@@ -27,13 +27,16 @@
 <header id="Menu">
 
 
-<nav class="topnav" id="myTopnav">
-  <a id="logo"><i class="fa"><img src="https://imgur.com/a/fIQC0sK"/></i></a> <!-- Botar a logo via html e editar via css -->
-  <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Inicio</a> 
-  <a href="#"><i class="fa fa-fw fa-info-circle" ></i> Sobre</a> 
-  <a href="#"><i class="fa fa-fw fa-envelope"></i> Contatos</a> 
+
+
+
+<div class="navbar">
+    <a id="logo"><i class="fa"><img src="https://imgur.com/a/fIQC0sK"/></i></a> <!-- Botar a logo via html e editar via css -->
+
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
   <div class="dropdown">
-    <button class="dropbtn">Serviço 
+    <button class="dropbtn">Dropdown 
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
@@ -41,13 +44,11 @@
       <a href="#">Link 2</a>
       <a href="#">Link 3</a>
     </div>
-     </div> 
-    <a href="#" id="login"><i class="fa fa-fw fa-user"></i> Login</a>
-    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-    
+  </div>
+      <a href="#" id="login"><i class="fa fa-fw fa-user"></i> Login</a>
  
-</nav>
+</div>
+
 <script>
 function myFunction() {
   var x = document.getElementById("myTopnav");

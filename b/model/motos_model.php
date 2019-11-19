7d5d4f86c71205c4908f7bd892e5e12f03d36702
Vinycius <53490812+viny724@users.noreply.github.com>
2019-11-19 17:21:40 -0200

    atualizado v3

--- b/model/motos_model.php
+++ b/model/motos_model.php
@@ -4,11 +4,7 @@
 		<title></title>
 	</head>
 	<body>
-		<form method="post" enctype="multipart/form-data" action="recebeUpload.php">
-   			Selecione uma imagem: <input name="arquivo" type="file" />
-   			<br />
-   			<input type="submit" value="Salvar" />
-		</form>
+		
 
 		<?php 
 		

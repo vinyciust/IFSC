76b9e3691b1b521b334d68e2a4e3074bed3c9cc2
gustavosilvani <gustavosilvani@hotmail.com>
2019-11-26 23:33:12 -0300

    Update fetch_data.php

--- b/controller/fetch_data.php
+++ b/controller/fetch_data.php
@@ -7,7 +7,8 @@ $connect = new PDO("mysql:host=localhost;dbname=realmotos", "root", "");
 $teste = new banco();
 $teste = $teste->conexao();
 $query = "SELECT * FROM tbl_sample ORDER BY id";
-$statement = $teste->query($query);
+$statement = $connect->query($query);
+
 if($statement->execute())
 {
 	while($row = $statement->fetch(PDO::FETCH_ASSOC))

8b5a67c332180fa36adb81df351326ecafaa3c94
Gustavo Silvani <gustavosilvani@hotmail.com>
2019-11-29 23:15:27 -0300

    aaa

--- b/view/viewSilvani.php
+++ b/view/viewSilvani.php
@@ -6,106 +6,163 @@ include 'menu.php';
 
 <section id="Produto">
 
-    <div class="vip-gallery-container ">
-    <div id="productGalleryCollection" class="vip-gallery" data-gallery="">
 
-        <span class="product-gallery-nav" data-direction="previous" style="">
-  <svg width="50" height="50" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" class="ui-icon ui-icon--chevron--left"><g fill="#3483fa" fill-rule="evenodd"><path d="M6.646 5.354l4 4 .354.353.707-.707-.353-.354-4-4L7 4.293 6.293 5z"></path><path d="M7.354 13.354l4-4L11.707 9 11 8.293l-.354.353-4 4-.353.354.707.707z"></path></g></svg>
-</span>
-        <span class="product-gallery-nav" data-direction="next" style="">
-  <svg width="50" height="50" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" class="ui-icon ui-icon--chevron--right"><g fill="#3483fa" fill-rule="evenodd"><path d="M6.646 5.354l4 4 .354.353.707-.707-.353-.354-4-4L7 4.293 6.293 5z"></path><path d="M7.354 13.354l4-4L11.707 9 11 8.293l-.354.353-4 4-.353.354.707.707z"></path></g></svg>
-</span>
 
-        <div id="gallery_dflt" class="product-gallery thumbs-col-1 ch-enlarge ch-points-ltrt" style="min-height: 500px">
+    <link rel="stylesheet" href="../src/css/lightslider.css">
+    <style>
+        ul{
+            list-style: none outside none;
+            padding-left: 0;
+            margin: 0;
+        }
+        .demo .item{
+            margin-bottom: 60px;
+        }
+        .content-slider li{
+            background-color: #ed3020;
+            text-align: center;
+            color: #FFF;
+        }
+        .content-slider h3 {
+            margin: 0;
+            padding: 70px 0;
+        }
+        .demo{
+            width: 800px;
+        }
+    </style>
+    <script src="../src/js/lightslider.js"></script> 
+    <script>
+         $(document).ready(function() {
+            $("#content-slider").lightSlider({
+                loop:true,
+                keyPress:true
+            });
+            $('#image-gallery').lightSlider({
+                gallery:true,
+                item:1,
+                thumbItem:9,
+                slideMargin: 0,
+                speed:500,
+                auto:true,
+                loop:true,
+                onSliderLoad: function() {
+                    $('#image-gallery').removeClass('cS-hidden');
+                }  
+            });
+        });
+    </script>
+<link type="text/css" rel="stylesheet" charset="UTF-8" href="https://translate.googleapis.com/translate_static/css/translateelement.css"></head>
 
-            <div class="gallery-content item-gallery__wrapper" data-gallery-id="default" data-full-images="[{&quot;src&quot;:&quot;https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_910958-MLB31830463824_082019-F.webp&quot;,&quot;w&quot;:&quot;546&quot;,&quot;h&quot;:&quot;352&quot;},{&quot;src&quot;:&quot;https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_731320-MLB31830462368_082019-F.webp&quot;,&quot;w&quot;:&quot;546&quot;,&quot;h&quot;:&quot;328&quot;},{&quot;src&quot;:&quot;https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_969273-MLB31830466137_082019-F.webp&quot;,&quot;w&quot;:&quot;546&quot;,&quot;h&quot;:&quot;327&quot;},{&quot;src&quot;:&quot;https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_911662-MLB31830457305_082019-F.webp&quot;,&quot;w&quot;:&quot;536&quot;,&quot;h&quot;:&quot;484&quot;},{&quot;src&quot;:&quot;https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_749880-MLB31830457066_082019-F.webp&quot;,&quot;w&quot;:&quot;252&quot;,&quot;h&quot;:&quot;520&quot;},{&quot;src&quot;:&quot;https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_952696-MLB32641556242_102019-F.webp&quot;,&quot;w&quot;:&quot;536&quot;,&quot;h&quot;:&quot;326&quot;}]">
-
-                <label for="thumbgallery_default-0" class="gallery__thumbnail  gallery__thumbnail--selected" data-imgindex="0">
-
-                    <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_Q_NP_910958-MLB31830463824_082019-R.webp" width="70" height="70">
-
-                </label>
-
-                <input type="radio" id="thumbgallery_default-0" class="thumbgallery_default-0" name="product-gallery-thumbnail_default" checked="">
-                <figure class="gallery-image-container thumbgallery_default-0">
-
-                    <a href="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_910958-MLB31830463824_082019-F.webp" class="gallery-trigger gallery-item--landscape " data-imgindex="0" data-size="546x352" style="height: 322px">
-                        <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_910958-MLB31830463824_082019-O.webp" width="500" height="322" alt="smart tv 4k samsung 58 visual livres hdr un58ru7100gxzd" data-imgindex="0" srcset="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_910958-MLB31830463824_082019-F.webp 2x">
-                    </a>
-                </figure>
-
-                <label for="thumbgallery_default-1" class="gallery__thumbnail  " data-imgindex="1">
-
-                    <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_Q_NP_731320-MLB31830462368_082019-R.webp" width="70" height="70">
-
-                </label>
-
-                <input type="radio" id="thumbgallery_default-1" class="thumbgallery_default-1" name="product-gallery-thumbnail_default">
-                <figure class="gallery-image-container thumbgallery_default-1">
-
-                    <a href="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_731320-MLB31830462368_082019-F.webp" class="gallery-trigger gallery-item--landscape " data-imgindex="1" data-size="546x328" style="height: 300px">
-                        <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_731320-MLB31830462368_082019-O.webp" width="500" height="300" alt="smart tv 4k samsung 58 visual livres hdr un58ru7100gxzd" data-imgindex="1" srcset="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_731320-MLB31830462368_082019-F.webp 2x">
-                    </a>
-                </figure>
-
-                <label for="thumbgallery_default-2" class="gallery__thumbnail  " data-imgindex="2">
-
-                    <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_Q_NP_969273-MLB31830466137_082019-R.webp" width="70" height="70">
-
-                </label>
-
-                <input type="radio" id="thumbgallery_default-2" class="thumbgallery_default-2" name="product-gallery-thumbnail_default">
-                <figure class="gallery-image-container thumbgallery_default-2">
-
-                    <a href="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_969273-MLB31830466137_082019-F.webp" class="gallery-trigger gallery-item--landscape " data-imgindex="2" data-size="546x327" style="height: 299px">
-                        <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_969273-MLB31830466137_082019-O.webp" width="500" height="299" alt="smart tv 4k samsung 58 visual livres hdr un58ru7100gxzd" data-imgindex="2" srcset="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_969273-MLB31830466137_082019-F.webp 2x">
-                    </a>
-                </figure>
-
-                <label for="thumbgallery_default-3" class="gallery__thumbnail  " data-imgindex="3">
-
-                    <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_Q_NP_911662-MLB31830457305_082019-R.webp" width="70" height="70">
-
-                </label>
-
-                <input type="radio" id="thumbgallery_default-3" class="thumbgallery_default-3" name="product-gallery-thumbnail_default">
-                <figure class="gallery-image-container thumbgallery_default-3">
-
-                    <a href="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_911662-MLB31830457305_082019-F.webp" class="gallery-trigger gallery-item--landscape " data-imgindex="3" data-size="536x484" style="height: 451px">
-                        <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_911662-MLB31830457305_082019-O.webp" width="500" height="451" alt="smart tv 4k samsung 58 visual livres hdr un58ru7100gxzd" data-imgindex="3" srcset="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_911662-MLB31830457305_082019-F.webp 2x">
-                    </a>
-                </figure>
-
-                <label for="thumbgallery_default-4" class="gallery__thumbnail  " data-imgindex="4">
-
-                    <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_Q_NP_749880-MLB31830457066_082019-R.webp" width="70" height="70">
-
-                </label>
-
-                <input type="radio" id="thumbgallery_default-4" class="thumbgallery_default-4" name="product-gallery-thumbnail_default">
-                <figure class="gallery-image-container thumbgallery_default-4">
-
-                    <a href="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_749880-MLB31830457066_082019-F.webp" class="gallery-trigger gallery-item--portrait " data-imgindex="4" data-size="252x520" style="height: 500px">
-                        <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_749880-MLB31830457066_082019-O.webp" width="242" height="500" alt="smart tv 4k samsung 58 visual livres hdr un58ru7100gxzd" data-imgindex="4" srcset="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_749880-MLB31830457066_082019-F.webp 2x">
-                    </a>
-                </figure>
-
-                <label for="thumbgallery_default-5" class="gallery__thumbnail  " data-imgindex="5">
-
-                    <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_Q_NP_952696-MLB32641556242_102019-R.webp" width="70" height="70">
-
-                </label>
-
-                <input type="radio" id="thumbgallery_default-5" class="thumbgallery_default-5" name="product-gallery-thumbnail_default">
-                <figure class="gallery-image-container thumbgallery_default-5">
-
-                    <a href="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_952696-MLB32641556242_102019-F.webp" class="gallery-trigger gallery-item--landscape " data-imgindex="5" data-size="536x326" style="height: 304px">
-                        <img src="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_952696-MLB32641556242_102019-O.webp" width="500" height="304" alt="smart tv 4k samsung 58 visual livres hdr un58ru7100gxzd" data-imgindex="5" srcset="https://http2.mlstatic.com/smart-tv-4k-samsung-58-visual-livres-hdr-un58ru7100gxzd-D_NQ_NP_952696-MLB32641556242_102019-F.webp 2x">
-                    </a>
-                </figure>
-
+    <div class="item">
+    <div class="clearfix" style="max-width:474px;">
+        <div class="lSSlideOuter ">
+            <div class="lSSlideWrapper usingCss" style="transition-duration: 500ms; transition-timing-function: ease;">
+                <ul id="image-gallery" class="gallery list-unstyled lightSlider lsGrab lSSlide" style="width: 8058px; transform: translate3d(-1422px, 0px, 0px); height: 343px; padding-bottom: 0%;">
+                    <li data-thumb="img/thumb/cS-15.jpg" class="clone left" style="width: 474px; margin-right: 0px;">
+                        <img src="../view/img/1.jpeg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-1.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="../view/img/2.jpeg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-2.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="../view/img/3.jpeg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-3.jpg" class="lslide active" style="width: 474px; margin-right: 0px;">
+                        <img src="../view/img/4.jpeg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-4.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-4.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-5.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-5.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-6.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-6.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-7.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-7.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-8.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-8.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-9.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-9.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-10.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-10.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-11.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-11.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-12.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-12.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-13.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-13.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-14.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-14.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-15.jpg" class="lslide" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-15.jpg">
+                    </li>
+                    <li data-thumb="img/thumb/cS-1.jpg" class="clone right" style="width: 474px; margin-right: 0px;">
+                        <img src="img/cS-1.jpg">
+                    </li>
+                </ul>
+                <div class="lSAction">
+                    <a class="lSPrev"></a>
+                    <a class="lSNext"></a>
+                </div>
             </div>
-
+            <ul class="lSPager lSGallery" style="margin-top: 5px; transition-duration: 500ms; width: 798.833px; transform: translate3d(0px, 0px, 0px);">
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px" class="">
+                    <a href="#"><img src="../view/img/1.jpeg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="../view/img/2.jpeg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px" class="active">
+                    <a href="#"><img src="../view/img/3.jpeg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="../view/img/4.jpeg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="img/thumb/cS-5.jpg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="img/thumb/cS-6.jpg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="img/thumb/cS-7.jpg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="img/thumb/cS-8.jpg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="img/thumb/cS-9.jpg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="img/thumb/cS-10.jpg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="img/thumb/cS-11.jpg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="img/thumb/cS-12.jpg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="img/thumb/cS-13.jpg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="img/thumb/cS-14.jpg"></a>
+                </li>
+                <li style="width:100%;width:48.22222222222222px;margin-right:5px">
+                    <a href="#"><img src="img/thumb/cS-15.jpg"></a>
+                </li>
+            </ul>
         </div>
     </div>
 </div>

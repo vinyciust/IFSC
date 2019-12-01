15a9ee8f4043b3f7d2057fb1530d44ea3f59d91a
Gustavo Silvani <gustavosilvani@hotmail.com>
2019-11-30 23:24:21 -0300

    up

--- b/model/dropMotoModel.php
+++ b/model/dropMotoModel.php
@@ -33,7 +33,110 @@ class geraPaginaProdutoModel{
         }		$i++;
 			}
 				
-        				echo "<tr>
+        				echo "
+		<section id='geral'>
+		<section id='Produto'>
+
+ 		<link href='//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' rel='stylesheet' id='bootstrap-css'>
+		<script src='//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js'></script>
+		<script src='//code.jquery.com/jquery-1.11.1.min.js'></script>
+
+<div class='container'>
+    <div class='row'>
+
+        <div class='col-md-9'>
+            <div id='carousel-main' class='carousel slide ' data-ride='carousel' data-interval='5000'>
+                <!-- Carousel items -->
+                <div class='carousel-inner'>
+                    <div class='active item'>
+                        <img src='img/cg150img1.jpg' class='img-responsive'>
+                    </div>
+                    <div class='item'>
+                        <img src='img/cg150img2.jpg' class='img-responsive'>
+                    </div>
+                    <div class='item'>
+                        <img src='img/cg150img3.jpg' class='img-responsive'>
+                    </div>
+					
+                </div>
+
+
+                <!-- Controls -->
+                <a class='left carousel-control' href='.carousel' role='button' data-slide='prev'>
+                    <span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>
+                    <span class='sr-only'>Previous</span>
+                </a>
+                <a class='right carousel-control' href='.carousel' role='button' data-slide='next'>
+                    <span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span>
+                    <span class='sr-only'>Next</span>
+                </a>
+            </div>
+        </div>
+
+        <div class='col-md-3'>
+            <div id='carousel-pager'class='carousel slide' data-ride='carousel' data-interval='1000'>
+                <!-- Carousel items -->
+                <div class='carousel-inner vertical'>
+                    <div class='active item'>
+                        <img src='img/cg150img1.jpg' class='img-responsive' data-target='#carousel-main' data-slide-to='0'>
+                    </div>
+                    <div class='item'>
+                        <img src='img/cg150img2.jpg' class='img-responsive' data-target='#carousel-main' data-slide-to='1'>
+                    </div>
+                    <div class='item'>
+                        <img src='img/cg150img3.jpg' class='img-responsive' data-target='#carousel-main' data-slide-to='2'>
+                   </div>
+                </div>
+                
+                <!-- Controls -->
+                <a class='left carousel-control' href='#carousel-pager' role='button' data-slide='prev'>
+                    <span class='glyphicon glyphicon-chevron-up' aria-hidden='true'></span>
+                    <span class='sr-only'>Previous</span>
+                </a>
+                <a class='right carousel-control' href='#carousel-pager' role='button' data-slide='next'>
+                    <span class='glyphicon glyphicon-chevron-down' aria-hidden='true'></span>
+                    <span class='sr-only'>Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+</section>
+
+
+<section id='aside'>
+
+<article id='dados'>
+<h1>Nome do Produto: ".$row["Nome"]."</h1>
+<h3><p>Valor: ".$row["Valor"]."</p>
+<p></p>
+<p>Descrição:".$row["Descricao"]."</br> Ano:".$row["Ano"]."
+</p></h3>
+</article>
+
+<article id='botaoCompra'>
+
+
+
+<button type='button' class='btn btn-light'>   Comprar   </button>
+
+
+
+</article>
+
+</section>
+
+
+</section>
+
+
+
+        				<tr>
         <td>".$row["Nome"]."</td>
         <td>".$row["Descricao"]."</td> </br>
         
